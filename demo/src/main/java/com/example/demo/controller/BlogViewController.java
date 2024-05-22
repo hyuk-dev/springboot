434169db7f8f53c269185580a72d86cc60f1c09f
이동혁 <139575548+hyuk-dev@users.noreply.github.com>
2024-05-22 07:38:35 +0000

    오류 해결

--- demo/src/main/java/com/example/demo/controller/BlogViewController.java
+++ demo/src/main/java/com/example/demo/controller/BlogViewController.java
@@ -1,6 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.demo.dto.ArticleListViewResponse;
+import com.example.demo.service.BlogService;
 
 import lombok.RequiredArgsConstructor;
 
