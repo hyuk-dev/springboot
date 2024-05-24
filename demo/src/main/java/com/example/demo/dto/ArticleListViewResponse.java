79e069e2a146e92a6829310fc576e353874ee032
이동혁 <139575548+hyuk-dev@users.noreply.github.com>
2024-05-24 01:35:47 +0000

    글쓰기 기능 추가 및 글 리스트 역순출력, 글 리스트 뷰에서 날짜 확인이 가능하도록 업데이트.

--- demo/src/main/java/com/example/demo/dto/ArticleListViewResponse.java
+++ demo/src/main/java/com/example/demo/dto/ArticleListViewResponse.java
@@ -1,5 +1,7 @@
 package com.example.demo.dto;
 
+import java.time.LocalDateTime;
+
 import com.example.demo.domain.Article;
 
 import lombok.Getter;
@@ -9,10 +11,12 @@ public class ArticleListViewResponse {
   private final Long id;
   private final String title;
   private final String content;
+  private final LocalDateTime createdAt;
 
   public ArticleListViewResponse(Article article){
     this.id = article.getId();
     this.title = article.getTitle();
     this.content = article.getContent();
+    this.createdAt = article.getCreatedAt();
   }
 }
